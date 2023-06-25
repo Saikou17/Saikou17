c327d9859b7c4c1a1410ba13cdcc821de56877a4
Saikou17 <a01783208@itesm.mx>
2023-06-24 18:54:56 -0600

    AgregoEjericicios

--- Programacion Orientada a Objetos/C++/Introduccion 22 Junio.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream> //Biblioteca para manipulacion de dato
-
-char getChar(int i){
-    return '0' + i;
-}
---
+++ Programacion Orientada a Objetos/C++/Introduccion22Junio.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include <iostream> //Biblioteca para manipulacion de dato
+
+char getChar(int i){
+    int ACSII = 0 + i;
+    std::cout << ACSII;
+    return 0;
+}
+
+int main (){
+    getChar(5);
+}
