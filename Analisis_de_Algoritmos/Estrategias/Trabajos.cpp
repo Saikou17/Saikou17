4be4a8af4644a44cd89414069ab4d14a7cee68e6
Saikou17 <a01783208@itesm.mx>
2023-08-22 14:46:00 -0600

    Algoritmos

--- Analisis_de_Algoritmos/Estrategias/Trabajos.cpp
+++ Analisis_de_Algoritmos/Estrategias/Trabajos.cpp
@@ -1,6 +1,7 @@
 //* Incluimos las librerias
 using namespace std;
 #include <iostream>
+#include <stdlib.h>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -11,26 +12,31 @@ using namespace std;
 
 //*Primero leemos el archivo de texto
 
-vector <string> leerArchivo(string archivo){
-    vector <string> contenido; //* Vector que guarda el vector completo
+vector <vector<string>> leerArchivo(string archivo){
+    vector <vector<string>> contenido; //* Vector que guarda el vectores de enteros completo
     ifstream csv(archivo); //* El ifstream es una clase que permite el modo lectura del archivo
     if(csv.is_open()){ //* Revisa si el archivo esta abierto
-        string linea; //* Vector que guarda una conexion de nodos
-        while(getline(csv,linea)){ //*
-            if(linea==","){
-                continue;//* Si la linea contiene un coma lo ignora y continua a la siguiente iteracion
-            }
-            contenido.push_back(stoi(linea));
+        string linea; //* Linea del documento (conexion entre nodos y su peso)
+        vector <string> nodos;//* Creamos un vector para guardar dos nodos
+        while(getline(csv,linea)){ //* Bucle que recorre todas las lineas
+            nodos.push_back(linea);
+            contenido.push_back(nodos);//* Agregamos los valores al final del arreglo principal
         }
-        csv.close();
+        
+        csv.close(); //* Cerramos la lectura
     }else{
         cout << "No se pudo leer el archivo" << endl;
     }
-    for(int i=0; i<contenido.size(); i++){
-        cout << contenido[i] << endl;
+    for(int i=0; i<contenido.size(); i++){ //* Imprimimos los elemntos del vector
+        for(int j=0; j<contenido[i].size(); j++){
+            cout << contenido[i][j];
+        }
+        cout << "\n";
     }
-    return contenido;
+    return contenido; //* Regresamos el vector de vectores
 }
+
+//* Segundo Algoritmo de Kruskal
 
 int main(){
     leerArchivo("Archivo de pruebas.txt");
