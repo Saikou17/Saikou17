afd24aa99b7b4d91b41f8773492a12c5637d91fe
Saikou17 <a01783208@itesm.mx>
2023-10-23 09:27:08 -0600

    Actualizacion Repo

--- Web/FullstackOpen/Part1/part1/src/index.js
+++ Web/FullstackOpen/Part1/part1/src/index.js
@@ -35,27 +35,52 @@ const Total = ({parts}) => {
   )
 }
 
+const History = ({allClicks}) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
 const App = () => { //Declaramos una variable constante (no se puede reasignar) que no recibe ningun parametro
   console.log("Creamos un componente")//Este es el print en javascript
   const course = 'Half Stack application development'
-  const [clicks,setClicks] = useState({left:0, right:0})
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
   const handleLeftClick = () => {
-    const newClicks = { 
-      left: clicks.left + 1, 
-      right: clicks.right 
-    }
-    setClicks(newClicks)
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
   }
 
   const handleRightClick = () => {
-    const newClicks = { 
-      left: clicks.left, 
-      right: clicks.right + 1 
-    }
-    setClicks(newClicks)
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
-  const decreaseOne = () => setCounter(counter-1);
-  const restart = () => setCounter(0);
+
+  const eventGood = () => setGood(good+1)
+  const eventNeutral=()=> setNeutral(neutral+1);
+  const eventBad= ()=> setBad(bad+1)
+
   const parts = [
     {
       name: 'Fundamentals of React',
@@ -78,10 +103,25 @@ const App = () => { //Declaramos una variable constante (no se puede reasignar) 
     <Header course={course}/>
     <Content parts={parts}/>
     <Total parts={parts}/>
-    {clicks.left}
-    <button onClick={handleLeftClick}>left</button>
-    <button onClick={handleRightClick}>right</button>
-    {clicks.right}
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text="left" />
+      <Button onClick={handleRightClick} text="right" />
+      {right} 
+      <History allClicks={allClicks} />
+    </div>
+    <div>
+      <h1>Give Feedback</h1>
+      <Button onClick={eventGood} text="Good"/>
+      <Button onClick={eventNeutral}text="Neutral"/>
+      <Button onClick={eventBad} text="Bad"/> 
+      <h1>Statics</h1>
+      <p>Good <b>{good}</b></p>
+      <p>Neutral <b>{neutral}</b></p>
+      <p>Bad <b>{bad}</b></p>
+      <p>Total: <b>{good + neutral + bad}</b></p>
+      <p>Percent Positive: <b>{(good/(good + neutral + bad))*100}</b></p>
+    </div>
   </div>
   )
 }
